1faf6895595541b3e1e8287350dc6b10a2a4250c
henshoster <hset2000@gmail.com>
2019-02-05 20:30:02 +0200

    handle unauthorized token in client side

--- frontend/src/app/category/category.component.ts
+++ frontend/src/app/category/category.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ApiConnectorService } from "../service/api-connector.service";
+import { HttpErrorResponse } from "@angular/common/http";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-category",
@@ -8,7 +10,7 @@ import { ApiConnectorService } from "../service/api-connector.service";
 })
 export class CategoryComponent implements OnInit {
   category = [];
-  constructor(private _api: ApiConnectorService) {}
+  constructor(private _api: ApiConnectorService, private _router: Router) {}
 
   ngOnInit() {
     this._api.getCategory().subscribe(
@@ -16,7 +18,13 @@ export class CategoryComponent implements OnInit {
         this.category = res;
         console.log(this.category);
       },
-      err => console.log(err)
+      err => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            this._router.navigate(["/login"]);
+          }
+        }
+      }
     );
   }
 }
